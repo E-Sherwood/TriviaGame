a347c66b69ebc0cabc68c02b791c8d53a1cf7826
e-sherwood <34667492+E-Sherwood@users.noreply.github.com>
2018-09-15 01:43:48 -0500

    mechanics of form submission

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+var gameTimer = setInterval(countdown, 1000)
+function countdown(){
+
+}
+$("#begin").click(function () {
+    $("#questionWrapper").show();
+    $("#begin").hide();
+    $("#header").hide();
+});
